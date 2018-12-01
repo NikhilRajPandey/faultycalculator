981991c4b8e7ff4a36e934b9374bb36e2eeed04a
NikhilRajPandey <44927308+NikhilRajPandey@users.noreply.github.com>
2018-12-01 19:21:07 +0530

    I have made this using python 3.6
    It is a faulty calculator if i input 45*3 or 56+9 or 56/6 then it answers wrong

---
+++ main3.py
@@ -0,0 +1,29 @@
+# making a faulty calculator
+
+# taking input from the user and changing into integer
+inp1 = input("Operator")
+
+# taking 2nd input from user
+inp2 = input("First number")
+
+# taking 3rd number
+inp3 = input("Last number")
+
+# making a new variable
+new_input = inp2 + inp1 + inp3
+
+# checking the input
+if new_input == "45*3":
+    print(555)
+elif new_input == "56+9":
+    print(77)
+elif new_input == "56/6":
+    print(4)
+elif inp1 == "*":
+    print(int(inp2) * int(inp3))
+elif inp1 == "+":
+    print(int(inp2) + int(inp3))
+elif inp1 == "-":
+    print(int(inp2) - int(inp3))
+elif inp1 == "/":
+    print(int(inp2) / int(inp3))
